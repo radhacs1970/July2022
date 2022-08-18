1630fb2030e7ef67a12d6539cb8eb33e7065b8b1
radhacs1970 <radhacs1970@users.noreply.github.com>
2022-08-18 16:06:50 +0530

    samplejdbc

---
+++ Readme.txt
@@ -0,0 +1,16 @@
+1. Craete aproject called samplejdbc
+  mvn archetype:generate
+  groupid - com.hexa
+  artifactid - samplejdbc
+
+1.  copy the myscripts.sql, if need change db name and 
+execute in mysql 
+2. Add the mysql dependency from git- pom.xml
+3. create a file calle Custselectdb.java in the com/hexa folder
+4. copying the content from git  to this file
+
+5. compiling using
+  mvn clean compile
+
+6. execute using
+  mvn exec:java -Dexec.mainClass=com.hexa.Custselectdb 
---
+++ samplejdbc/myscript.sql
@@ -0,0 +1,22 @@
+use july2022;
+
+drop table customer;
+
+CREATE TABLE Customer(
+Cus_id varchar(255) Primary Key,
+Cus_name varchar(255),
+Cus_phone varchar(255),
+Cus_Email varchar(255),
+Cus_walletbal integer,
+Cus_Login_id integer,
+Cus_Password varchar(255)
+);
+INSERT INTO Customer VALUES
+('C0001','Sheldon','9434346142','Sheldon@gmail.com', 10000,100,'abcd'),
+('C0002','Amy','9800641110','Amy@gmail.com', 5500,101,'xyz'),
+('C0003','Leonard','9800456987','Leonard@gmail.com', 8700,102,'pqrs'),
+('C0004','Penny','9434346589','Penny@gmail.com',9000,103,'mnop'),
+('C0005','Harvard','9800262887','Harvard@gmail.com',4500,104,'ijkl');
+
+
+select * from customer;
